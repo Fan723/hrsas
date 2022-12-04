e49734559c01531971611831fff7eef4877ddc2d
723 <723gggff@qq.com>
2022-12-05 01:33:49 +0800

    路由页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,19 +9,17 @@
       label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">
-          <img src="@/assets/common/login-logo.png" alt="" />
-        </h3>
+        <img src="@/assets/common/login-logo.png" alt="" />
       </div>
 
       <el-form-item prop="mobile">
         <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
+          <svg-icon icon-class="user"> </svg-icon
+        ></span>
         <el-input
           ref="mobile"
           v-model="loginForm.mobile"
-          placeholder="请输入用户名"
+          placeholder="请输入手机号"
           name="mobile"
           type="text"
           tabindex="1"
@@ -52,16 +50,16 @@
       </el-form-item>
 
       <el-button
-        class="loginBtn"
         :loading="loading"
         type="primary"
+        class="loginBtn"
         style="width: 100%; margin-bottom: 30px"
         @click.native.prevent="handleLogin"
-        >Login</el-button
+        >登录</el-button
       >
 
       <div class="tips">
-        <span style="margin-right: 20px">账号: 13800000002</span>
+        <span style="margin-right: 20px">手机号: 13800000002</span>
         <span> 密码: 123456</span>
       </div>
     </el-form>
@@ -70,30 +68,37 @@
 
 <script>
 import { validMobile } from '@/utils/validate'
-
+import { mapActions } from 'vuex'
 export default {
   name: 'Login',
   data() {
     const validateMobile = (rule, value, callback) => {
-      if (!validMobile(value)) {
-        callback(new Error('手机号格式不正确'))
-      } else {
-        callback()
-      }
+      // rule 规则 value 输入的值  callback 用来给视图中展示具体信息 callback(new Error())
+      // 正则.test(要校验的字符串)，如果字符串被正则匹配成功，那么test方法返回true，否则返回false
+      validMobile(value)
+        ? callback()
+        : callback(new Error('请输入正确的手机号'))
     }
     return {
+      // 要绑定的数据对象
       loginForm: {
         mobile: '13800000002',
         password: '123456'
       },
       loginRules: {
         mobile: [
-          { required: true, trigger: 'blur', message: '用户名不能为空' },
-          { trigger: 'blur', validator: validateMobile }
+          // { required: true, trigger: 'blur', validator: validateUsername }
+          { required: true, trigger: 'blur', message: '请输入手机号' },
+          { validator: validateMobile, trigger: 'blur' }
         ],
         password: [
-          { required: true, trigger: 'blur', message: '密码不能为空' },
-          { min: 2, max: 10, message: '密码必须是2-10位' }
+          { required: true, trigger: 'blur', message: '请输入密码' },
+          {
+            min: 6,
+            max: 16,
+            trigger: 'blur',
+            message: '请输入长度为6-16位的密码'
+          }
         ]
       },
       loading: false,
@@ -110,6 +115,7 @@ export default {
     }
   },
   methods: {
+    ...mapActions(['user/login']),
     showPwd() {
       if (this.passwordType === 'password') {
         this.passwordType = ''
@@ -120,15 +126,24 @@ export default {
         this.$refs.password.focus()
       })
     },
+    // 给登录按钮绑定的方法
     async handleLogin() {
       try {
+        // 1.  表单格式校验
         await this.$refs.loginForm.validate()
+        // 如果代码能够继续往下走，那么就表示验证成功，否则就是验证失败
+        // 验证成功之后就进行登录
+        this.loading = true
+        // 2. 发起登录请求
         await this.$store.dispatch('user/login', this.loginForm)
+        // 如果登录成功就会继续执行后面的代码
+        // 3. 页面进行跳转
         this.$router.push('/')
       } catch (err) {
         console.log(err)
+      } finally {
+        this.loading = false
       }
-      this.loading = false
     }
   }
 }
@@ -150,8 +165,10 @@ $cursor: #fff;
 
 /* reset element-ui css */
 .login-container {
+  // 给页面加上背景
   background-image: url('~@/assets/common/login.jpg'); // 设置背景图片
-  background-position: center; // 将图片位置设置为充满整个屏幕
+  background-position: center;
+  background-size: cover;
   .el-input {
     display: inline-block;
     height: 47px;
@@ -180,9 +197,11 @@ $cursor: #fff;
     border-radius: 5px;
     color: #454545;
   }
+
   .el-form-item__error {
     color: #fff;
   }
+
   .loginBtn {
     background: #407ffe;
     height: 64px;
@@ -234,6 +253,7 @@ $light_gray: #eee;
 
   .title-container {
     position: relative;
+    margin-bottom: 44px;
 
     .title {
       font-size: 26px;
