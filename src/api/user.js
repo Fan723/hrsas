8567bf73030ac52780cd2eb2520b3f56e1987ec0
723 <723gggff@qq.com>
2022-12-05 01:01:49 +0800

    主页框架拦截登出实现

--- src/api/user.js
+++ src/api/user.js
@@ -7,7 +7,19 @@ export function login(data) {
     data
   })
 }
-
+// 获取用户基本资料
+export function getUserInfo() {
+  return request({
+    url: '/sys/profile',
+    method: 'post'
+  })
+}
+// 获取用户头像
+export function getUserDetailById(id) {
+  return request({
+    url: `/sys/user/${id}`
+  })
+}
 export function getInfo(token) {}
 
 export function logout() {}
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,10 @@
-import { getToken, removeToken, setToken } from '@/utils/auth'
-import { login } from '@/api/user'
+import { getToken, removeToken, setTimeStamp, setToken } from '@/utils/auth'
+import { getUserDetailById, getUserInfo, login } from '@/api/user'
 export default {
   namespaced: true,
   state: {
-    token: getToken() || ''
+    token: getToken() || '',
+    userInfo: {}
   },
   mutations: {
     SET_TOKEN(state, token) {
@@ -13,13 +14,39 @@ export default {
     REMOVE_TOKEN(state) {
       state.token = '' // 删除vuex的token
       removeToken() // 先清除 vuex  再清除缓存 vuex和 缓存数据的同步
+    },
+    SET_USER_INFO(state, userInfo) {
+      state.userInfo = { ...userInfo }
+    },
+    REMOVE_USERINFO(state) {
+      state.userInfo = {}
     }
   },
   actions: {
     async login(context, data) {
       const res = await login(data)
-      console.log(res)
+      // console.log(res)
       context.commit('SET_TOKEN', res)
+      // 写入时间戳
+      setTimeStamp // 将当前的最新时间写入缓存
+    },
+    // 获取用户的资料
+    async getUserInfo(context) {
+      const result = await getUserInfo() // result就是用户的基本资料
+      const baseInfo = await getUserDetailById(result.userId) // 为了获取头像
+      const baseResult = { ...result, ...baseInfo } // 将两个接口结果合并
+      // 此时已经获取到了用户的基本资料 迫不得已 为了头像再次调用一个接口
+      context.commit('SET_USER_INFO', baseResult) // 提交mutations
+      // 加一个点睛之笔  这里这一步，暂时用不到，但是请注意，这给我们后边会留下伏笔
+      return baseResult
+    },
+    // TODO 登出功能我还不会
+    // 登出的action
+    logout(context) {
+      // 删除token
+      context.commit('REMOVE_TOKEN') // 不仅仅删除了vuex中的 还删除了缓存中的
+      // 删除用户资料
+      context.commit('REMOVE_USERINFO') // 删除用户信息
     }
   }
 }
